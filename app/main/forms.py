1179f44e1e9b242ca10224e6d550c86b5be41efe
Joseph Ghanimah <jfghanimah@gmail.com>
2020-09-23 00:18:53 -0500

    added contact us form

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=1, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=1, max=40)])
+    subject = StringField('Subject', validators=[DataRequired(), Length(min=1, max=100)])    
+    message = TextAreaField('Message', validators=[DataRequired(), Length(min=1, max=1000)])

7c9d997652fe19ea43e4b35347b6d6db9f71c9b5
Joseph Ghanimah <jfghanimah@gmail.com>
2020-10-13 06:15:59 -0500

    updated contact form validators

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired, Length, Email
 
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=1, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=1, max=40)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=4, max=40)])
     subject = StringField('Subject', validators=[DataRequired(), Length(min=1, max=100)])    
-    message = TextAreaField('Message', validators=[DataRequired(), Length(min=1, max=1000)])
+    message = TextAreaField('Message', validators=[DataRequired(), Length(min=1, max=8000)])
